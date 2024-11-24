4a05700d50f7e89e7962e226eb1fe8fe3c4b799d
Izundu Clara Chinecherem <ci.chinecherem@stu.unizik.edu.ng>
2024-11-24 16:37:28 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+print("icarly character Creator")
+myCharacter=input("Do you own a web show:")
+if myCharacter=="yes":
+  print("Then you are Carly")
+else:
+  print("Then you are not carly")
+myName=input("Are you a blonde:")
+if myName=="yes":
+  print("Then you are Sam")
+else:
+  print("Then you are not Sam")
+myjob=input("Are you a sculptor:")
+if myjob=="yes":
+  print("Then you are Spencer")
+else:
+  print("who then are you?:")
+  
